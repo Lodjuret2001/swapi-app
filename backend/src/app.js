5ceb657768b3523bbf7329e2259a9edc9653a57f
Pontus <minecraftpontus@gmail.com>
2024-02-27 19:10:37 +0100

    Added public folder and added CORS to server

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+import cors from "cors";
+app.use(cors());
+
 import mongoose from "mongoose";
 mongoose
   .connect("mongodb://localhost:27017/swapi-app")
